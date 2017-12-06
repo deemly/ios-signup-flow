743c6eded722bcf6e4f3e83bff19c967bcfafd3d
Anders Borum <anders@algoritmer.dk>
2017-12-06 12:22:40 +0100

    example sign up flow using WKWebView

--- Deemly Login/ViewController.swift
+++ Deemly Login/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.emailField.becomeFirstResponder()
+    }
+    
+    func refreshButton() {
+        let emailValid = (emailField.text ?? "").contains("@")
+        let nameValid = (nameField.text ?? "").count > 0
+        signUpButton.isEnabled = emailValid && nameValid
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: UITextFieldDelegate
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // refresh button state in next run-loop when text contains changed values
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+            self.refreshButton()
+            
+        }
+
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailField {
+            nameField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        
+        return true
     }
 
-
+    // MARK: -
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? WebViewController {
+            destination.email = (emailField.text ?? "").trimmingCharacters(in: .whitespaces)
+            destination.name = (nameField.text ?? "").trimmingCharacters(in: .whitespaces)
+        }
+    }
 }
-
---
+++ Deemly Login/WebViewController.swift
@@ -0,0 +1,95 @@
+//
+//  WebViewController.swift
+//  Deemly Login
+//
+//  Created by Anders Borum on 06/12/2017.
+//  Copyright © 2017 Anders Borum. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController, WKNavigationDelegate {
+    
+    public var email = ""
+    public var name = ""
+    public var redirectUrl = URL(string: "http://laernogetnyt.dk/")!
+    
+    @IBOutlet weak var webView: WKWebView!
+
+    func quoteJavascript(string: String) -> String {
+        var escaped = string.replacingOccurrences(of: "\\", with: "\\\\")
+        escaped = escaped.replacingOccurrences(of: "\"", with: "\\\"")
+        escaped = escaped.replacingOccurrences(of: "\n", with: "\\n")
+        escaped = escaped.replacingOccurrences(of: "\r", with: "\\r")
+        return "\"\(escaped)\""
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        webView.navigationDelegate = self
+        
+        // load sign-up page from resource bundle
+        guard let url = Bundle.main.url(forResource: "signup", withExtension: "html") else { return }
+        guard var string = try? String.init(contentsOf: url) else { return }
+
+        // insert known values for email, name and returnUrl
+        string = string.replacingOccurrences(of: "$(EMAIL)", with: quoteJavascript(string: email))
+        string = string.replacingOccurrences(of: "$(NAME)", with: quoteJavascript(string: name))
+        string = string.replacingOccurrences(of: "$(RETURNURL)", with: quoteJavascript(string: redirectUrl.absoluteString))
+        
+        // run as static html page that submits itself to start flow
+        webView.loadHTMLString(string, baseURL: nil)
+    }
+
+    // called when webView redirects to the redirectUrl and the app might dismiss the webView
+    func flowCompleted() {
+        let title = NSLocalizedString("Done", comment: "")
+        let message = NSLocalizedString("You are back at redirectUrl which indicates sign up flow is done or aborted by user.", comment: "")
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default,
+                                      handler: { _ in
+
+            // dismiss web view
+            self.navigationController?.popViewController(animated: true)
+        }))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func show(error: Error) {
+        let title = NSLocalizedString("Error", comment: "")
+        let message = error.localizedDescription
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil))
+
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: WKNavigationDelegate
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        show(error: error)
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        show(error: error)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction,
+                 decisionHandler: @escaping (WKNavigationActionPolicy) -> Swift.Void) {
+        
+        print("decidePolicyFor: \(navigationAction.request.url?.absoluteString ?? "")")
+        
+        // we assume flow is completed as soon as we are back at the hostname of the redirectUrl
+        // and more precise checks might be needed
+        if navigationAction.request.url?.host == redirectUrl.host {
+            flowCompleted()
+        }
+        
+        decisionHandler(.allow)
+    }
+
+}
