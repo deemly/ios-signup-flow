5ef656371370cdf1861cbf1d8ff3b7d74e0d627a
Anders Borum <anders@algoritmer.dk>
2018-01-09 13:35:21 +0100

    switched to external browser workflow where exceptions are thrown if configuration or url handling is bad

--- Deemly Login/AppDelegate.swift
+++ Deemly Login/AppDelegate.swift
@@ -13,7 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        if Deemly.open(url: url) { return true }
+        return false
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
